44b7a188365e42600fd83e82e77ba68da2e8c5f7
isaacs <i@izs.me>
2019-03-12 23:44:15 -0700

    Update tap to v13-rc2, cleanup some snapshots

--- test/test-point.jsx
+++ test/test-point.jsx
@@ -4,17 +4,15 @@ const importJSX = require('import-jsx')
 const TestPoint = importJSX('../lib/test-point.js')
 const {render} = require('ink-testing-library')
 
-const clean = str => str
-  .replace(/^  stack: \|(\n    .*)+/gm, '  {stack}')
-
 const r = render(<div />)
 
 const test = new t.Test()
 test.name = 'some name'
 test.parser.on('assert', res => {
   res.testName = test.name
+  res.diag && res.diag.stack && (res.diag.stack = '{stack}')
   r.rerender(<TestPoint res={res} />)
-  t.matchSnapshot(clean(r.lastFrame()), res.name)
+  t.matchSnapshot(r.lastFrame(), res.name)
 })
 
 test.pass('this is fine')
