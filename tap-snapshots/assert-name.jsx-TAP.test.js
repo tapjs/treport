9a2c50fd234b027208fe784aa0eeb9573e8a4ae6
isaacs <i@izs.me>
2019-03-12 17:26:24 -0700

    test for assert-name tag

---
+++ tap-snapshots/assert-name.jsx-TAP.test.js
@@ -0,0 +1,150 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`assert-name.jsx TAP > {"ok":true,"skip":false,"todo":false,"name":"name"} 1`] = `
+   name
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":false,"todo":false} 1`] = `
+
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":false,"todo":true,"name":"name"} 1`] = `
+ ☐ name
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":false,"todo":true} 1`] = `
+ ☐
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":false,"todo":"todo reason","name":"name"} 1`] = `
+ ☐ name > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":false,"todo":"todo reason"} 1`] = `
+ ☐  > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":true,"todo":false,"name":"name"} 1`] = `
+ ~ name
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":true,"todo":false} 1`] = `
+ ~
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":true,"todo":true,"name":"name"} 1`] = `
+ ~ name
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":true,"todo":true} 1`] = `
+ ~
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":true,"todo":"todo reason","name":"name"} 1`] = `
+ ~ name > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":true,"todo":"todo reason"} 1`] = `
+ ~  > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":"skip reason","todo":false,"name":"name"} 1`] = `
+ ~ name > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":"skip reason","todo":false} 1`] = `
+ ~  > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":"skip reason","todo":true,"name":"name"} 1`] = `
+ ~ name > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":"skip reason","todo":true} 1`] = `
+ ~  > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":"skip reason","todo":"todo reason","name":"name"} 1`] = `
+ ~ name > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":true,"skip":"skip reason","todo":"todo reason"} 1`] = `
+ ~  > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":false,"todo":false,"name":"name"} 1`] = `
+ ! name
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":false,"todo":false} 1`] = `
+ !
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":false,"todo":true,"name":"name"} 1`] = `
+ ☐ name
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":false,"todo":true} 1`] = `
+ ☐
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":false,"todo":"todo reason","name":"name"} 1`] = `
+ ☐ name > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":false,"todo":"todo reason"} 1`] = `
+ ☐  > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":true,"todo":false,"name":"name"} 1`] = `
+ ~ name
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":true,"todo":false} 1`] = `
+ ~
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":true,"todo":true,"name":"name"} 1`] = `
+ ~ name
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":true,"todo":true} 1`] = `
+ ~
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":true,"todo":"todo reason","name":"name"} 1`] = `
+ ~ name > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":true,"todo":"todo reason"} 1`] = `
+ ~  > todo reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":"skip reason","todo":false,"name":"name"} 1`] = `
+ ~ name > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":"skip reason","todo":false} 1`] = `
+ ~  > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":"skip reason","todo":true,"name":"name"} 1`] = `
+ ~ name > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":"skip reason","todo":true} 1`] = `
+ ~  > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":"skip reason","todo":"todo reason","name":"name"} 1`] = `
+ ~ name > skip reason
+`
+
+exports[`assert-name.jsx TAP > {"ok":false,"skip":"skip reason","todo":"todo reason"} 1`] = `
+ ~  > skip reason
+`
---
+++ test/assert-name.jsx
@@ -0,0 +1,26 @@
+const React = require('react')
+const importJSX = require('import-jsx')
+const t = require('tap')
+const AssertName = importJSX('../lib/assert-name.js')
+const {render} = require('ink-testing-library')
+
+const ok = [ true, false ]
+const skip = [false, true, 'skip reason']
+const todo = [false, true, 'todo reason']
+const name = ['name', undefined]
+const cases = []
+
+ok.forEach(ok =>
+  skip.forEach(skip =>
+    todo.forEach(todo =>
+      name.forEach(name => cases.push({ok, skip, todo, name})))))
+
+let r
+for (let i = 0; i < cases.length; i++) {
+  if (r)
+    r.rerender(<AssertName {...cases[i]} />)
+  else
+    r = render(<AssertName {...cases[i]} />)
+  t.matchSnapshot(r.lastFrame(), JSON.stringify(cases[i]))
+}
+r.unmount()
