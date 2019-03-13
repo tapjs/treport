66cc3a9969aa34e8d36703fbb3e04f6a0d19026b
isaacs <i@izs.me>
2019-03-12 17:58:09 -0700

    tests of the counter string

---
+++ test/counts.js
@@ -0,0 +1,45 @@
+const t = require('tap')
+// tell chalk to be colorful
+process.argv.push('--color')
+const counts = require('../lib/counts.js')
+
+const cases = [
+  [0,0,0,0],
+  [0,0,0,1],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,1,0,0],
+  [0,1,0,1],
+  [0,1,1,0],
+  [0,1,1,1],
+  [1,0,0,0],
+  [1,0,0,1],
+  [1,0,1,0],
+  [1,0,1,1],
+  [1,1,0,0],
+  [1,1,0,1],
+  [1,1,1,0],
+  [1,1,1,1],
+  [0,0,0,2],
+  [0,0,2,0],
+  [0,0,2,2],
+  [0,2,0,0],
+  [0,2,0,2],
+  [0,2,2,0],
+  [0,2,2,2],
+  [2,0,0,0],
+  [2,0,0,2],
+  [2,0,2,0],
+  [2,0,2,2],
+  [2,2,0,0],
+  [2,2,0,2],
+  [2,2,2,0],
+  [2,2,2,2],
+]
+
+cases.forEach(set => {
+  const [pass, fail, todo, skip] = set
+  const total = pass + fail + todo + skip
+  t.matchSnapshot(counts({pass, fail, todo, skip, total}),
+    JSON.stringify({pass, fail, todo, skip, total}))
+})
