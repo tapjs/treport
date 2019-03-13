ecb8b7e9baf9107a26fe86627993c922877f992d
isaacs <i@izs.me>
2019-03-12 22:31:08 -0700

    PassFail test

---
+++ test/pass-fail.jsx
@@ -0,0 +1,11 @@
+const React = require('react')
+const t = require('tap')
+const importJSX = require('import-jsx')
+const PassFail = importJSX('../lib/pass-fail.js')
+const {render} = require('ink-testing-library')
+
+const r = render(<PassFail ok={true} name="this is fine" />)
+t.matchSnapshot(r.lastFrame(), 'with a name')
+r.rerender(<PassFail />)
+t.matchSnapshot(r.lastFrame(), 'no name')
+r.unmount()
