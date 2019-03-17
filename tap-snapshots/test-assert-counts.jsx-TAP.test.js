7203a09d1521259573902d877859edde2f03726e
isaacs <i@izs.me>
2019-03-16 20:36:10 -0700

    Split suite and assert counts from summary, add tests

---
+++ tap-snapshots/test-assert-counts.jsx-TAP.test.js
@@ -0,0 +1,70 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":0,"todo":0,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m0 [22m[39mof 0[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":0,"todo":0,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[36m1 skip[39m[22m[39m, [22m[39mof 1[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":0,"todo":1,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m[35m1 todo[39m[22m[39m, [22m[39mof 1[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":0,"todo":1,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[35m1 todo[39m[22m[39m, [22m[39m[36m1 skip[39m[22m[39m, [22m[39mof 2[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":1,"todo":0,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m[32m1 passed[39m[22m[39m, [22m[39mof 1[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":1,"todo":0,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[32m1 passed[39m[22m[39m, [22m[39m[36m1 skip[39m[22m[39m, [22m[39mof 2[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":1,"todo":1,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m[32m1 passed[39m[22m[39m, [22m[39m[35m1 todo[39m[22m[39m, [22m[39mof 2[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":0,"pass":1,"todo":1,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[32m1 passed[39m[22m[39m, [22m[39m[35m1 todo[39m[22m[39m, [22m[39m[36m1 skip[39m[22m[39m, [22m[39mof 3[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":0,"todo":0,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39mof 1[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":0,"todo":0,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39m[36m1 skip[39m[22m[39m, [22m[39mof 2[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":0,"todo":1,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39m[35m1 todo[39m[22m[39m, [22m[39mof 2[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":0,"todo":1,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39m[35m1 todo[39m[22m[39m, [22m[39m[36m1 skip[39m[22m[39m, [22m[39mof 3[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":1,"todo":0,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39m[32m1 passed[39m[22m[39m, [22m[39mof 2[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":1,"todo":0,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39m[32m1 passed[39m[22m[39m, [22m[39m[36m1 skip[39m[22m[39m, [22m[39mof 3[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":1,"todo":1,"skip":0} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39m[32m1 passed[39m[22m[39m, [22m[39m[35m1 todo[39m[22m[39m, [22m[39mof 3[22m
+`
+
+exports[`test/assert-counts.jsx TAP > {"fail":1,"pass":1,"todo":1,"skip":1} 1`] = `
+[1mAsserts:[22m  [39m[31m1 failed[39m[22m[39m, [22m[39m[32m1 passed[39m[22m[39m, [22m[39m[35m1 todo[39m[22m[39m, [22m[39m[36m1 skip[39m[22m[39m, [22m[39mof 4[22m
+`
