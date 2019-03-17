4b1b019a0c079bb7f5f848e3597995aefbbd51dc
isaacs <i@izs.me>
2019-03-16 22:58:00 -0700

    Abstract out tag for all the real time logging

---
+++ test/log.jsx
@@ -0,0 +1,13 @@
+const importJSX = require('import-jsx')
+const React = require('react')
+const Log = importJSX('../lib/log.js')
+const t = require('tap')
+const {render} = require('ink-testing-library')
+
+const r = render(<Log log={[
+  {raw: 'hello'},
+  {result: {ok: true, name: 'this is fine', testName: 'foo'}},
+  {result: {ok: false, name: 'not fine', testName: 'bar'}},
+]} />)
+
+t.matchSnapshot(r.lastFrame())
