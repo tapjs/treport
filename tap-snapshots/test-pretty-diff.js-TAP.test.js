b9609b09c3813d98caa0f66bb2b5b64a88d7b0bb
isaacs <i@izs.me>
2019-03-16 21:08:04 -0700

    test for pretty-diff

---
+++ tap-snapshots/test-pretty-diff.js-TAP.test.js
@@ -0,0 +1,33 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/pretty-diff.js TAP > a pretty diff at 40 columns 1`] = `
+[48;2;34;34;34m  [49m[38;2;51;0;0m[48;2;255;153;204m--- expected                        [49m[39m
+[48;2;34;34;34m  [49m[38;2;0;51;0m[48;2;204;255;153m+++ actual                          [49m[39m
+[48;2;34;34;34m  [49m[48;2;255;255;255m[38;2;17;17;17m Object {                           [39m[49m
+[48;2;34;34;34m  [49m[38;2;0;51;0m[48;2;204;255;153m+  "a a a a a a a a a a a a a a a a a a a a a a a a a a a a a": 1,[49m[39m
+[48;2;34;34;34m  [49m[38;2;51;0;0m[48;2;255;153;204m-  "b": 2,                          [49m[39m
+[48;2;34;34;34m  [49m[48;2;255;255;255m[38;2;17;17;17m }                                  [39m[49m
+`
+
+exports[`test/pretty-diff.js TAP > a pretty diff at 2 columns 1`] = `
+[48;2;34;34;34m  [49m[38;2;51;0;0m[48;2;255;153;204m--- expected[49m[39m
+[48;2;34;34;34m  [49m[38;2;0;51;0m[48;2;204;255;153m+++ actual[49m[39m
+[48;2;34;34;34m  [49m[48;2;255;255;255m[38;2;17;17;17m Object {[39m[49m
+[48;2;34;34;34m  [49m[38;2;0;51;0m[48;2;204;255;153m+  "a a a a a a a a a a a a a a a a a a a a a a a a a a a a a": 1,[49m[39m
+[48;2;34;34;34m  [49m[38;2;51;0;0m[48;2;255;153;204m-  "b": 2,[49m[39m
+[48;2;34;34;34m  [49m[48;2;255;255;255m[38;2;17;17;17m }[39m[49m
+`
+
+exports[`test/pretty-diff.js TAP > a pretty diff at 0 columns 1`] = `
+[48;2;34;34;34m  [49m[38;2;51;0;0m[48;2;255;153;204m--- expected                                                       [49m[39m
+[48;2;34;34;34m  [49m[38;2;0;51;0m[48;2;204;255;153m+++ actual                                                         [49m[39m
+[48;2;34;34;34m  [49m[48;2;255;255;255m[38;2;17;17;17m Object {                                                          [39m[49m
+[48;2;34;34;34m  [49m[38;2;0;51;0m[48;2;204;255;153m+  "a a a a a a a a a a a a a a a a a a a a a a a a a a a a a": 1, [49m[39m
+[48;2;34;34;34m  [49m[38;2;51;0;0m[48;2;255;153;204m-  "b": 2,                                                         [49m[39m
+[48;2;34;34;34m  [49m[48;2;255;255;255m[38;2;17;17;17m }                                                                 [39m[49m
+`
